71c5f7cada01d94028565bc003d294e3dda49b0e
thugperfect <meenamani9444@gmail.com>
2023-08-31 14:37:50 +0530

    body styles

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,23 +1,105 @@
 const Body = () =>{
 return(
-    <div className=" container bg-gray-500 bg-opacity-30  h-[100vh] mx-auto w-[80%]">
-        <div className="flex  overflow-x-scroll gap-2">
-        <div className="flex gap-3">
-        <div className="w-[150px] h-[200px] bg-black rounded-lg m-5"></div>
-        <div className="w-[150px] h-[200px] bg-black rounded-lg m-5 "></div>
-        <div className="w-[150px] h-[200px] bg-black rounded-lg m-5"></div>
-        <div className="w-[150px] h-[200px] bg-black rounded-lg m-5"></div>
-        <div className="w-[150px] h-[200px] bg-black rounded-lg m-5"></div>
-        <div className="w-[150px] h-[200px] bg-black rounded-lg m-5"></div>
-        <div className="w-[150px] h-[200px] bg-black rounded-lg m-5"></div>
-        <div className="w-[150px] h-[200px] bg-black rounded-lg m-5"></div>
-        <div className="w-[150px] h-[200px] bg-black rounded-lg m-5"></div>
-        <div className="w-[150px] h-[200px] bg-black rounded-lg m-5"></div>
-        <div className="w-[150px] h-[200px] bg-black rounded-lg m-5"></div>
-        <div className="w-[150px] h-[200px] bg-black rounded-lg m-5"></div>
-        <div className="w-[150px] h-[200px] bg-black rounded-lg m-5"></div>
-        <div className="w-[150px] h-[200px] bg-black rounded-lg m-5"></div>
+    <div className=" container bg-gray-500 bg-opacity-30  h-auto mx-auto w-[80%]">
+         <div className="text-xl text-white px-5">Latest Movies</div>
+         <div className="flex  overflow-x-scroll gap-2 transparent-scroll">
+        <div className="flex ">
+        <div className="w-[150px] h-[200px] bg-black rounded-lg m-3"></div>
+        <div className="w-[150px] h-[200px] bg-black rounded-lg m-3"></div>
+        <div className="w-[150px] h-[200px] bg-black rounded-lg m-3"></div>
+        <div className="w-[150px] h-[200px] bg-black rounded-lg m-3"></div>
+        <div className="w-[150px] h-[200px] bg-black rounded-lg m-3"></div>
+        <div className="w-[150px] h-[200px] bg-black rounded-lg m-3"></div>
+        <div className="w-[150px] h-[200px] bg-black rounded-lg m-3"></div>
+        <div className="w-[150px] h-[200px] bg-black rounded-lg m-3"></div>
+        <div className="w-[150px] h-[200px] bg-black rounded-lg m-3"></div>
+        <div className="w-[150px] h-[200px] bg-black rounded-lg m-3"></div>
+        <div className="w-[150px] h-[200px] bg-black rounded-lg m-3"></div>
+        <div className="w-[150px] h-[200px] bg-black rounded-lg m-3"></div>
+        <div className="w-[150px] h-[200px] bg-black rounded-lg m-3"></div>
+        <div className="w-[150px] h-[200px] bg-black rounded-lg m-3"></div>
             </div>
+            
+       
+        </div>
+        <div className="text-xl text-white px-5">Action</div>
+        <div className="flex  overflow-x-scroll transparent-scroll">
+        <div className="flex gap-3">
+        <div className="w-[150px] h-[200px] bg-black rounded-lg m-3"></div>
+        <div className="w-[150px] h-[200px] bg-black rounded-lg m-3 "></div>
+        <div className="w-[150px] h-[200px] bg-black rounded-lg m-3"></div>
+        <div className="w-[150px] h-[200px] bg-black rounded-lg m-3"></div>
+        <div className="w-[150px] h-[200px] bg-black rounded-lg m-3"></div>
+        <div className="w-[150px] h-[200px] bg-black rounded-lg m-3"></div>
+        <div className="w-[150px] h-[200px] bg-black rounded-lg m-3"></div>
+        <div className="w-[150px] h-[200px] bg-black rounded-lg m-3"></div>
+        <div className="w-[150px] h-[200px] bg-black rounded-lg m-3"></div>
+        <div className="w-[150px] h-[200px] bg-black rounded-lg m-3"></div>
+        <div className="w-[150px] h-[200px] bg-black rounded-lg m-3"></div>
+        <div className="w-[150px] h-[200px] bg-black rounded-lg m-3"></div>
+        <div className="w-[150px] h-[200px] bg-black rounded-lg m-3"></div>
+        <div className="w-[150px] h-[200px] bg-black rounded-lg m-3"></div>
+            </div>
+            
+       
+        </div>
+        <div className="text-xl text-white px-5">Romance</div>
+        <div className="flex  overflow-x-scroll transparent-scroll">
+        <div className="flex gap-3">
+        <div className="w-[150px] h-[200px] bg-black rounded-lg m-3"></div>
+        <div className="w-[150px] h-[200px] bg-black rounded-lg m-3 "></div>
+        <div className="w-[150px] h-[200px] bg-black rounded-lg m-3"></div>
+        <div className="w-[150px] h-[200px] bg-black rounded-lg m-3"></div>
+        <div className="w-[150px] h-[200px] bg-black rounded-lg m-3"></div>
+        <div className="w-[150px] h-[200px] bg-black rounded-lg m-3"></div>
+        <div className="w-[150px] h-[200px] bg-black rounded-lg m-3"></div>
+        <div className="w-[150px] h-[200px] bg-black rounded-lg m-3"></div>
+        <div className="w-[150px] h-[200px] bg-black rounded-lg m-3"></div>
+        <div className="w-[150px] h-[200px] bg-black rounded-lg m-3"></div>
+        <div className="w-[150px] h-[200px] bg-black rounded-lg m-3"></div>
+        <div className="w-[150px] h-[200px] bg-black rounded-lg m-3"></div>
+        <div className="w-[150px] h-[200px] bg-black rounded-lg m-3"></div>
+        <div className="w-[150px] h-[200px] bg-black rounded-lg m-3"></div>
+            </div>
+            
+       
+        </div>
+        <div className="text-xl text-white px-5">Comedy</div>
+        <div className="flex  overflow-x-scroll transparent-scroll">
+        <div className="flex gap-3">
+        <div className="w-[150px] h-[200px] bg-black rounded-lg m-3"></div>
+        <div className="w-[150px] h-[200px] bg-black rounded-lg m-3 "></div>
+        <div className="w-[150px] h-[200px] bg-black rounded-lg m-3"></div>
+        <div className="w-[150px] h-[200px] bg-black rounded-lg m-3"></div>
+        <div className="w-[150px] h-[200px] bg-black rounded-lg m-3"></div>
+        <div className="w-[150px] h-[200px] bg-black rounded-lg m-3"></div>
+        <div className="w-[150px] h-[200px] bg-black rounded-lg m-3"></div>
+        <div className="w-[150px] h-[200px] bg-black rounded-lg m-3"></div>
+        <div className="w-[150px] h-[200px] bg-black rounded-lg m-3"></div>
+        <div className="w-[150px] h-[200px] bg-black rounded-lg m-3"></div>
+        <div className="w-[150px] h-[200px] bg-black rounded-lg m-3"></div>
+        <div className="w-[150px] h-[200px] bg-black rounded-lg m-3"></div>
+        <div className="w-[150px] h-[200px] bg-black rounded-lg m-3"></div>
+        <div className="w-[150px] h-[200px] bg-black rounded-lg m-3"></div>
+            </div>
+            <div className="text-xl text-white px-5">Horror</div>
+        <div className="flex  overflow-x-scroll transparent-scroll">
+        <div className="flex gap-3">
+        <div className="w-[150px] h-[200px] bg-black rounded-lg m-3"></div>
+        <div className="w-[150px] h-[200px] bg-black rounded-lg m-3 "></div>
+        <div className="w-[150px] h-[200px] bg-black rounded-lg m-3"></div>
+        <div className="w-[150px] h-[200px] bg-black rounded-lg m-3"></div>
+        <div className="w-[150px] h-[200px] bg-black rounded-lg m-3"></div>
+        <div className="w-[150px] h-[200px] bg-black rounded-lg m-3"></div>
+        <div className="w-[150px] h-[200px] bg-black rounded-lg m-3"></div>
+        <div className="w-[150px] h-[200px] bg-black rounded-lg m-3"></div>
+        <div className="w-[150px] h-[200px] bg-black rounded-lg m-3"></div>
+        <div className="w-[150px] h-[200px] bg-black rounded-lg m-3"></div>
+        <div className="w-[150px] h-[200px] bg-black rounded-lg m-3"></div>
+        <div className="w-[150px] h-[200px] bg-black rounded-lg m-3"></div>
+        <div className="w-[150px] h-[200px] bg-black rounded-lg m-3"></div>
+        <div className="w-[150px] h-[200px] bg-black rounded-lg m-3"></div>
+            </div></div>
             
        
         </div>
--- src/components/Topbar.js
+++ src/components/Topbar.js
@@ -3,8 +3,11 @@ const Topbar = () =>{
     return(
        <div className="w-full">
          <div className="h-[80px] bg-zinc-900 w-full flex items-center px-5 justify-between">
+            <div className="flex">
             <div className="w-[60px] h-[60px] bg-red-50 rounded-full"></div>
-            <div className="text-[35px] text-green-400">Movie Fetch</div>
+            <div className="text-[35px] px-5 text-green-400">Movie Fetch</div>
+            </div>
+   
             <div className="flex gap-2">
                 <input className=" px-2 outline-0 rounded-[5px] w-full lg:w-[300px] md:w-[100px]  " type="text" placeholder="Search for Movies" ></input>
                 <div className="text-gray-500 hover:text-gray-600 cursor-pointer">Home</div>
