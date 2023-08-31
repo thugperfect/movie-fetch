16b69e7a65a021a0fe2b7394ee71d70abd344fd0
thugperfect <meenamani9444@gmail.com>
2023-08-31 10:29:02 +0530

    styles

--- src/components/Topbar.js
+++ src/components/Topbar.js
@@ -5,7 +5,7 @@ const Topbar = () =>{
          <div className="h-[80px] bg-zinc-900 w-full flex items-center px-5 justify-between">
             <div className="w-[60px] h-[60px] bg-red-50 rounded-full"></div>
             <div className="flex gap-2">
-                <input className=" rounded-[5px] w-full lg:w-[100px] md:w-[300px]  " type="text" placeholder="Search for Movies" ></input>
+                <input className=" rounded-[5px] w-full lg:w-[300px] md:w-[100px]  " type="text" placeholder="Search for Movies" ></input>
                 <div className="text-gray-500 hover:text-gray-600 cursor-pointer">Home</div>
                 <div className="text-gray-500 hover:text-gray-600 cursor-pointer">About</div>
                 <div className="text-gray-500 hover:text-gray-600 cursor-pointer">Login</div>
