085810eda2913d8184ccfcbbbb1a790e803c844a
thugperfect <meenamani9444@gmail.com>
2023-08-31 20:35:26 +0530

    latest_movies complete

--- src/components/MovieBox.js
+++ src/components/MovieBox.js
@@ -1,6 +1,10 @@
-const MovieBox = ({movieName,moviePoster}) =>{
+const MovieBox = ({movieName}) =>{
+    if(movieName)
+    console.log(movieName);
     return(
         <div className="w-[150px] h-[200px] bg-black text-white rounded-lg m-3">
+
+           { (movieName)?<img className="w-full h-full rounded-lg" src={movieName.Poster} alt={movieName.Title}/>:""}
             
         </div>
     )
