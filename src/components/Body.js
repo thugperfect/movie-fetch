b685d48f73df9ebd94bfa12c2761b046ac9cc1e6
thugperfect <meenamani9444@gmail.com>
2023-08-31 20:13:44 +0530

    separated all genres

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,34 +1,70 @@
 import MovieBox from "./MovieBox";
 import {useState,useEffect} from 'react'
 const Body = () => {
-  const api_key = 'http://www.omdbapi.com/?i=tt3896198&apikey=68ec0e63'
+
+  const [latest,setLatest] = useState()
+  const [action,setAction] = useState()
+  const [romance,setRomance] = useState()
+  const [comedy,setComedy] = useState()
+  const [horror,setHorror] = useState()
+  const search="action";
+  const title = "a";
+  const year = 2023
+  const api_key = `http://www.omdbapi.com/?s=${search}&y=${year}&apikey=68ec0e63`
+  const latest_api_key = `http://www.omdbapi.com/?s=action&y=2023&apikey=68ec0e63`
+  const action_api_key = `http://www.omdbapi.com/?s=action&apikey=68ec0e63`
+  const romance_api_key = `http://www.omdbapi.com/?s=romance&apikey=68ec0e63`
+  const comedy_api_key = `http://www.omdbapi.com/?s=comedy&apikey=68ec0e63`
+  const horror_api_key = `http://www.omdbapi.com/?s=horror&apikey=68ec0e63`
+
 
 
   useEffect(()=>{
     const fetchMovie =async () =>{
-      const movies =await fetch(api_key)
-      const cont = await movies.json()
-      console.log(cont);
+      const latest_movies =await fetch(latest_api_key)
+      const action_movies =await fetch(action_api_key)
+      const romance_movies =await fetch(romance_api_key)
+      const comedy_movies =await fetch(comedy_api_key)
+      const horror_movies =await fetch(horror_api_key)
+      const latest_cont = await latest_movies.json()
+      const action_cont = await action_movies.json()
+      const romance_cont = await romance_movies.json()
+      const comedy_cont = await comedy_movies.json()
+      const horror_cont = await horror_movies.json()
+     setLatest(latest_cont.Search)
+     setAction(action_cont.Search)
+     setRomance(romance_cont.Search)
+     setComedy(comedy_cont.Search)
+     setHorror(horror_cont.Search)
     }
     fetchMovie()
   },[])
+  
+
   return (
     <div className=" container bg-gray-500 bg-opacity-30  h-auto mx-auto w-[80%]">
       <div className="text-2xl pt-5 cursor-pointer font-bold text-white px-5">Latest Movies</div>
       <div className="flex  overflow-x-scroll  transparent-scroll">
-        <div className="flex ">
-          <MovieBox />
-          <MovieBox />
-          <MovieBox />
-          <MovieBox />
-          <MovieBox />
-          <MovieBox />
-          <MovieBox />
-          <MovieBox />
-          <MovieBox />
-          <MovieBox />
-          <MovieBox />
-        </div>
+        
+
+          {
+            latest.length>0?
+            <div className="flex ">
+            </div>:<div className="flex ">
+              <MovieBox/>
+              <MovieBox/>
+              <MovieBox/>
+              <MovieBox/>
+              <MovieBox/>
+              <MovieBox/>
+              <MovieBox/>
+              <MovieBox/>
+              <MovieBox/>
+              <MovieBox/>
+            </div>
+          }
+
+       
       </div>
       <div className="text-2xl pt-5 cursor-pointer font-bold text-white px-5">Action</div>
       <div className="flex  overflow-x-scroll transparent-scroll">
@@ -111,7 +147,7 @@ const Body = () => {
           </div>
         </div>
       </div>
-   
+
   );
 };
 export default Body;
--- src/components/MovieBox.js
+++ src/components/MovieBox.js
@@ -1,4 +1,4 @@
-const MovieBox = () =>{
+const MovieBox = ({movieName,moviePoster}) =>{
     return(
         <div className="w-[150px] h-[200px] bg-black rounded-lg m-3"></div>
     )
