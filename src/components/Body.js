49bd83e5fdf31ff7b855daa992e8e96705791cce
thugperfect <meenamani9444@gmail.com>
2023-08-31 14:55:01 +0530

    separated moviebox

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,93 +1,104 @@
-import MovieBox from "./Moviebox"
-const Body = () =>{
-return(
+import MovieBox from "./MovieBox";
+const Body = () => {
+  return (
     <div className=" container bg-gray-500 bg-opacity-30  h-auto mx-auto w-[80%]">
-         <div className="text-xl text-white px-5">Latest Movies</div>
-         <div className="flex  overflow-x-scroll  transparent-scroll">
+      <div className="text-2xl font-bold text-white px-5">Latest Movies</div>
+      <div className="flex  overflow-x-scroll  transparent-scroll">
         <div className="flex ">
-        <MovieBox/>
-        <MovieBox/>
-        <MovieBox/>
-        <MovieBox/>
-        <MovieBox/>
-        <MovieBox/>
-        <MovieBox/>
-        <MovieBox/>
-        <MovieBox/>
-        <MovieBox/>
-        <MovieBox/>
-            </div>
+          <MovieBox />
+          <MovieBox />
+          <MovieBox />
+          <MovieBox />
+          <MovieBox />
+          <MovieBox />
+          <MovieBox />
+          <MovieBox />
+          <MovieBox />
+          <MovieBox />
+          <MovieBox />
         </div>
-        <div className="text-xl text-white px-5">Action</div>
-        <div className="flex  overflow-x-scroll transparent-scroll">
+      </div>
+      <div className="text-xl font-bold text-white px-5">Action</div>
+      <div className="flex  overflow-x-scroll transparent-scroll">
         <div className="flex gap-3">
-       <MovieBox/>
-       <MovieBox/>
-       <MovieBox/>
-       <MovieBox/>
-       <MovieBox/>
-       <MovieBox/><MovieBox/>
-       <MovieBox/>
-       <MovieBox/>
-       <MovieBox/>
-       <MovieBox/>
-       <MovieBox/>
-       <MovieBox/>
-
-            </div>
-            
-       
+          <MovieBox />
+          <MovieBox />
+          <MovieBox />
+          <MovieBox />
+          <MovieBox />
+          <MovieBox />
+          <MovieBox />
+          <MovieBox />
+          <MovieBox />
+          <MovieBox />
+          <MovieBox />
+          <MovieBox />
+          <MovieBox />
         </div>
-        <div className="text-xl text-white px-5">Romance</div>
-        <div className="flex  overflow-x-scroll transparent-scroll">
+      </div>
+      <div className="text-xl font-bold text-white px-5">Romance</div>
+      <div className="flex  overflow-x-scroll transparent-scroll">
         <div className="flex ">
-        <MovieBox/>
-        <MovieBox/>
-        <MovieBox/>
-        <MovieBox/><MovieBox/>
-        <MovieBox/>
-        <MovieBox/>
-        <MovieBox/><MovieBox/>
+          <MovieBox />
+          <MovieBox />
+          <MovieBox />
+          <MovieBox />
+          <MovieBox />
+          <MovieBox />
+          <MovieBox />
+          <MovieBox />
+          <MovieBox />
 
-        <MovieBox/><MovieBox/>
-        <MovieBox/>
-        <MovieBox/><MovieBox/>
-            </div>
-            
-       
+          <MovieBox />
+          <MovieBox />
+          <MovieBox />
+          <MovieBox />
+          <MovieBox />
         </div>
-        <div className="text-xl text-white px-5">Comedy</div>
-        <div className="flex  overflow-x-scroll transparent-scroll">
+      </div>
+      <div className="text-xl font-bold text-white px-5">Comedy</div>
+      <div className="flex  overflow-x-scroll transparent-scroll">
         <div className="flex ">
-        <MovieBox/>
-        <MovieBox/><MovieBox/><MovieBox/>
-        <MovieBox/>
-        <MovieBox/>
-        <MovieBox/><MovieBox/>
-        <MovieBox/><MovieBox/><MovieBox/><MovieBox/><MovieBox/><MovieBox/>
+          <MovieBox />
+          <MovieBox />
+          <MovieBox />
+          <MovieBox />
+          <MovieBox />
+          <MovieBox />
+          <MovieBox />
+          <MovieBox />
+          <MovieBox />
+          <MovieBox />
+          <MovieBox />
+          <MovieBox />
+          <MovieBox />
+          <MovieBox />
 
-        <MovieBox/><MovieBox/><MovieBox/>
-            </div>
-            <div className="text-xl text-white px-5">Horror</div>
-        <div className="flex  overflow-x-scroll transparent-scroll">
-        <div className="flex gap-3">
-        <MovieBox/><MovieBox/><MovieBox/><MovieBox/><MovieBox/>
-
-
-        <MovieBox/>
-        <MovieBox/>
-        <MovieBox/>
-        <MovieBox/>
-        <MovieBox/>
-        <MovieBox/>
-        <MovieBox/>
-        <MovieBox/><MovieBox/>
-        
-            </div></div>
-            
-       
+          <MovieBox />
+          <MovieBox />
+          <MovieBox />
         </div>
+        <div className="text-xl font-bold text-white px-5">Horror</div>
+        <div className="flex  overflow-x-scroll transparent-scroll">
+          <div className="flex ">
+            <MovieBox />
+            <MovieBox />
+            <MovieBox />
+            <MovieBox />
+            <MovieBox />
+            <MovieBox />
+            <MovieBox />
+            <MovieBox />
+            <MovieBox />
+            <MovieBox />
+            <MovieBox />
+            <MovieBox />
+            <MovieBox />
+            <MovieBox />
+          </div>
+        </div>
+      </div>
     </div>
-)
-}
-export default Body
+  );
+};
+export default Body;
