fb9a3308b9312fa3ad785cb52d78c387e8b6fdc7
thugperfect <meenamani9444@gmail.com>
2023-09-01 12:58:04 +0530

    pulse animate complete

--- src/components/MovieBox.js
+++ src/components/MovieBox.js
@@ -1,10 +1,11 @@
 const MovieBox = ({movieName,text}) =>{
   
     return(
-        <div className="resp h-[200px] w-[150px]  text-white rounded-lg m-3 cursor-pointer">
-            {(text)?<div className="w-full h-full flex justify-center items-center text-[20px] bg-black">{text}...</div>:""}
+        <div className="resp h-[200px] w-[150px]  text-white  m-3 cursor-pointer">
+            {(text)?<div className="w-full h-full flex rounded-lg justify-center items-center text-[20px] bg-black">{text}...</div>:""}
            {(movieName)?<img className="w-full h-full rounded-lg" src={movieName.Poster} alt={movieName.Title}/>:
-           ""}
+           (!text)?
+           <div className=" animate-pulse bg-black w-full h-full rounded-lg"></div>:""}
 
             
         </div>
